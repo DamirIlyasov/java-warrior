9c9dc1b4b2e6e2ef7669f76ba0fbdb7b5c2a4b91
DamirIlyasov <damirilyasov1997@gmail.com>
2018-04-20 21:53:42 +0300

    [5]: Created empty project

---
+++ src/main/java/ru/itis/javawarrior/JavaWarriorApplication.java
@@ -0,0 +1,12 @@
+package ru.itis.javawarrior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWarriorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWarriorApplication.class, args);
+	}
+}
---
+++ src/main/java/ru/itis/javawarrior/controller/MainController.java
@@ -0,0 +1,20 @@
+package ru.itis.javawarrior.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Damir Ilyasov
+ */
+@RestController
+public class MainController {
+  @ApiOperation("Index page")
+  @GetMapping("/")
+  public ResponseEntity<String> index() {
+    return new ResponseEntity<>("Server online!", HttpStatus.OK);
+  }
+}
