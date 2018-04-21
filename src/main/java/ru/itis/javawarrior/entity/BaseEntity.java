763dd7feb99794af4ec609af26db22815b51ece0
Хайруллин Азат < >
2018-04-21 11:03:36 +0300

    [5] - Добавлен BaseDAO, BaseEntity, исправлено кол-во пробелов

---
+++ src/main/java/ru/itis/javawarrior/entity/BaseEntity.java
@@ -0,0 +1,16 @@
+package ru.itis.javawarrior.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    protected Long id;
+}
