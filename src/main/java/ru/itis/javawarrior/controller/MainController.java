91190839221fd1aa88890f64ab6221a39ed2e432
Хайруллин Азат < >
2018-04-28 12:41:31 +0300

    [30] - Заменил RequestParam на RequestBody

--- src/main/java/ru/itis/javawarrior/controller/MainController.java
+++ src/main/java/ru/itis/javawarrior/controller/MainController.java
@@ -6,11 +6,9 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.itis.javawarrior.entity.GameResult;
+import ru.itis.javawarrior.json.CompileJson;
 import ru.itis.javawarrior.service.CompileService;
 import ru.itis.javawarrior.util.ActionEnum;
 
@@ -30,8 +28,8 @@ public class MainController {
 
     @ApiOperation("Test compile. Now can compile only walk(), jump() and attack()")
     @PostMapping("/compile")
-    public ResponseEntity<GameResult> testCompile(@RequestParam("inputtedCode") String inputtedCode) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-        GameResult result = compileService.compile(inputtedCode);
+    public ResponseEntity<GameResult> testCompile(@RequestBody CompileJson compileJson) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        GameResult result = compileService.compile(compileJson.getInputtedCode());
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
