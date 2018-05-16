af14c0c284955bebbb4aedcfd8bcb105a8e99946
Хайруллин Азат < >
2018-05-16 16:22:05 +0300

    [40] - Ability to use methods twice removed

--- src/main/java/ru/itis/javawarrior/util/messages/Messages.java
+++ src/main/java/ru/itis/javawarrior/util/messages/Messages.java
@@ -5,6 +5,7 @@ public class Messages {
     public static final String VALIDATION_ERROR= "Серверная ошибка валидации";
     public static final String UNACCEPTABLE_CODE = "Вы используете запрещенные конструкции";
     public static final String INVALID_CODE = "Код не компилируется, допущена ошибка : \n";
+    public static final String METHOD_COUNT_USAGE_ERROR = "Нельзя использовать каждый метод больше одного раза";
 
     //Successes
     public static final String VALIDATION_SUCCESS = "Валидация успешна";
