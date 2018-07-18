db8eb128c783e0581401b19c83342b8a62f250b3
Хайруллин Азат < >
2018-07-18 21:07:59 +0300

    [bug-fix] - swagger fix

--- src/main/java/ru/itis/javawarrior/config/SecurityConfig.java
+++ src/main/java/ru/itis/javawarrior/config/SecurityConfig.java
@@ -8,12 +8,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableOAuth2Sso
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- swagger ui
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // other public endpoints of your API may be appended to this array
+            "/",
+            "/login**",
+            "/error**"
+    };
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().cors().and()
                 .authorizeRequests()
 //                .antMatchers("/**").permitAll()
-                .antMatchers("/", "/login**", "/webjars/**", "/error**", "/swagger-ui**", "/swagger-resources/**")
+                .antMatchers(AUTH_WHITELIST)
                 .permitAll()
                 .antMatchers("/compile**", "/user**", "/level**")
                 .authenticated()
