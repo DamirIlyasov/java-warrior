ae8806ac157ab996b913e0d562eadc49cdedda2d
Хайруллин Азат < >
2018-07-23 13:38:40 +0300

    [bug-fix] - testCompile file edited

--- src/main/java/ru/itis/javawarrior/service/FileService.java
+++ src/main/java/ru/itis/javawarrior/service/FileService.java
@@ -3,7 +3,7 @@ package ru.itis.javawarrior.service;
 import java.io.IOException;
 
 public interface FileService {
-    String TEST_FILE_PATH = "TestFile.java";
+    String TEST_FILE_PATH = "src/main/java/ru/itis/javawarrior/util/compile/TestFile.java";
     String TEST_FILE_NAME = "TestFile";
     void writeToFile(String code) throws IOException;
 }
